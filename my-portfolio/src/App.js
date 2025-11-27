7b282ea92daa0a808865db15376cef3886b5c7ed
Shaina <shainaz@udel.edu>
2025-11-27 13:03:14 -0500

    updated info

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -13,23 +13,32 @@ export default function Portfolio() {
     }}>
 
     </div>
-      {/* Header */}
       <header className="header">
         <h1 className="main-title">Hi, I'm Shaina Zaccagnino</h1>
         <p className="subtitle">Computer Science Student & Aspiring Developer</p>
       </header>
 
-       {/* Education */}
       <section className="section">
         <h2 className="section-title">Education</h2>
         <div className="card-container">
           <div className="card">
+            <p>M.S. in Computer Science, University of Delaware, 2027</p>
             <p>B.A. in Computer Science, University of Delaware, 2026</p>
             <p>B.A. in Game Studies and Esports, University of Delaware, 2026</p>
           </div>
           </div>
         </section>
-      {/* Experience */}
+      <section className="section">
+        <h2 className="section-title">Skills</h2>
+         <div className="card-container">
+          <div className="card">
+            <p className="experience-title">Programming Languages</p>
+            <p>Swift, Python, JavaScript, TypeScript, C, C++, C#, HTML </p>
+           <p className="experience-title">Software</p>
+           <p>Adobe Photoshop, Adobe Illustrator, Microsoft Office, Blender, Unity </p>
+          </div>
+          </div>
+      </section>
       <section className="section">
         <h2 className="section-title">Experience</h2>
         <div className="card-container">
@@ -39,7 +48,7 @@ export default function Portfolio() {
               Performed individual market research on invention disclosures<br/>
               Consulted with business partners to market technology<br/>
               Lead a project to design a nationwide exercise program</p>
-            <p className="experience-title">IOS Developer Intern, Earth Hero: May 2025-Present</p>
+            <p className="experience-title">IOS Developer Intern, Earth Hero: May 2025-July 2025</p>
             <p className="experience-body">Developed and maintained the native iOS application using Swift and SwiftUI<br/>
               Optimized app performance and memory usage<br/>
               Translated user needs into technical solutions and app features<br/>
@@ -54,7 +63,6 @@ export default function Portfolio() {
         </div>
       </section>
 
-      {/* Projects */}
       <section className="section">
         <h2 className="section-title">Projects</h2>
         <div className="card-container">
@@ -64,7 +72,7 @@ export default function Portfolio() {
             <a href="https://shaina-z.github.io/CISC275-Final-Project/" className="link-button">View on GitHub</a>
           </div>
           <div className="card">
-            <h3 className="card-title">Git Cookin</h3>
+            <h3 className="card-title">Git Cookin'</h3>
             <p>A game to teach comp sci students all about git commands!</p>
             <a href="https://github.com/Shaina-Z/Git-Cookin-" className="link-button">View on GitHub</a>
           </div>
