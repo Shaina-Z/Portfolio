c79d7d1e2c3a8cd20b38379d00d917522f9f3db7
Shaina-Z <shaina.zacc@gmail.com>
2026-05-28 11:30:37 -0400

    added new projects and description

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -15,9 +15,18 @@ export default function Portfolio() {
     </div>
       <header className="header">
         <h1 className="main-title">Hi, I'm Shaina Zaccagnino</h1>
-        <p className="subtitle">Computer Science Student & Aspiring Developer</p>
+        <p className="subtitle">Computer Science Student & Aspiring Game/Software Developer</p>
       </header>
-
+    <section className="section">
+        <h2 className="section-title">About Me</h2>
+        <div className="card-container">
+          <div className="card">
+            <p>Hello! I am currently a graduate student at the University of Delaware, pursuing a Master's degree in Computer Science. I am devoted to creating immersive experiences through game and software development. I have worked on many 
+              things, from games, to web applications, to mobile apps. Through my work, I hope to bring meaningful experiences and inspire others.
+               I encourage you to explore all of it!</p>
+          </div>
+        </div>
+      </section>
       <section className="section">
         <h2 className="section-title">Education</h2>
         <div className="card-container">
@@ -35,7 +44,7 @@ export default function Portfolio() {
             <p className="experience-title">Programming Languages</p>
             <p>Swift, SQL, Python, JavaScript, TypeScript, C, C++, C#, HTML </p>
            <p className="experience-title">Software</p>
-           <p>Adobe Photoshop, Adobe Illustrator, Microsoft Office, Blender, Unity </p>
+           <p>Adobe Photoshop, Adobe Illustrator, Microsoft Office, Blender, Unity, Godot </p>
           </div>
           </div>
       </section>
@@ -69,14 +78,24 @@ export default function Portfolio() {
       <section className="section">
         <h2 className="section-title">Projects</h2>
         <div className="card-container">
+            <div className="card">
+            <h4 className="card-title">Tails of the Tower</h4>
+            <p>A hack and slash game where you traverse through a tower in order to defeat the rat king! Made with Godot.</p>
+            <a href="https://udel.itch.io/tails-of-the-tower" className="link-button">Play on Itch.io</a>
+          </div>
           <div className="card">
+            <h3 className="card-title">CommonGround</h3>
+            <p>A game made to engage students in learning about health and physical activity. Made with Unity.</p>
+            <a href="https://champions-d1706.web.app/?classroomId=ea3d551b-f250-46d8-97ed-a61493f5cf43" className="link-button">Play the Demo</a>
+          </div>
+           <div className="card">
             <h3 className="card-title">Career Helpi</h3>
             <p>A web app that helps users find the right career path for them using React, Node, and OpenAI API.</p>
             <a href="https://github.com/Shaina-Z/CISC275-Final-Project/" className="link-button">View on GitHub</a>
             <p></p>
             <a href="https://shaina-z.github.io/CISC275-Final-Project/" className="link-button">View Online</a>
           </div>
-            <div className="card">
+          <div className="card">
             <h4 className="card-title">Comic Chameleon</h4>
             <p>A drawing app where users can contribute to different comics together. 
               Made with React, Node, Nest.js, Tanstack, and the Konva API.
@@ -84,16 +103,6 @@ export default function Portfolio() {
             <a href="https://github.com/UD-CISC474-F25/f25-cisc474-pink" className="link-button">View on Github</a>
             <p></p>
             <a href="https://comic-chameleon.shainaz.workers.dev/" className="link-button">View Online</a>
-          </div>
-          <div className="card">
-            <h3 className="card-title">Git Cookin'</h3>
-            <p>A game to teach computer science students all about git commands!</p>
-            <a href="https://github.com/Shaina-Z/Git-Cookin-" className="link-button">View on GitHub</a>
-          </div>
-          <div className="card">
-            <h4 className="card-title">Oversleep Guard</h4>
-            <p>A prototype build of an andriod app meant to prevent oversleeping through GPS sensors.</p>
-            <a href="https://github.com/okitamisan01/OversleepGuard" className="link-button">View on GitHub</a>
           </div>
         </div>
       </section>
